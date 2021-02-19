31596f2df95077270c1128a9e20fa16c005da17e
eich47 <eich@tut.by>
2021-02-19 15:53:27 +0300

    feat: implement the crateDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) {
+    return false;
+  }
+  const memberList = members.filter((memberName) => {
+    if (typeof memberName !== "string") {
+      return false;
+    }
+    return memberName;
+  });
+
+  const res = memberList.map((memberName) => {
+    const splittedNames = memberName.trim().split(" ");
+
+    const nameMember = splittedNames[0];
+
+    return nameMember[0].toUpperCase();
+  });
+
+  return res.sort().join("");
 };
