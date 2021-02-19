3f4c0e6f86dc6327684d77b183a4e9a36cf64aa1
eich47 <eich@tut.by>
2021-02-19 21:33:07 +0300

    feat: implement the transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arrSeq) {
+  if (!(arrSeq instanceof Array)) {
+    throw new Error();
+  }
+
+  const sequences = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev",
+  ];
+  let result = [];
+
+  let arr = arrSeq.filter((item) => true);
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    const item = arr[idx];
+
+    if (typeof item === "number") {
+      result.push(item);
+    } else if (typeof item === "string") {
+      if (item === "--discard-next") {
+        const nextInd = idx + 1;
+        if (nextInd !== undefined) {
+          idx++;
+        }
+      } else if (item === "--discard-prev") {
+        if (idx !== 0) {
+          result.pop();
+        }
+      } else if (item === "--double-next") {
+        const nextEl = arr[idx + 1];
+        if (nextEl !== undefined && !(nextEl instanceof Array)) {
+          result.push(nextEl);
+        }
+      } else if (item === "--double-prev") {
+        const prevEl = arr[idx - 1];
+        if (prevEl !== undefined && !(prevEl instanceof Array)) {
+          result.push(prevEl);
+        }
+      } else {
+        result.push(item);
+      }
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
 };
